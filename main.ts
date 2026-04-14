8476ea98efd50bce141794c5b68e4a64cef2629a
Seanmarccomix <smccomics5@gmail.com>
2026-04-14 12:18:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,13 @@ basic.forever(function () {
     PingUnit.Centimeters
     )) {
         music.play(music.stringPlayable("G - G - G - G - ", 140), music.PlaybackMode.UntilDone)
-    } else {
+    } else if (10 > sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P0,
+    PingUnit.Centimeters
+    )) {
         music.play(music.stringPlayable("B C B C B C B C ", 170), music.PlaybackMode.UntilDone)
+    } else {
+        music.stopAllSounds()
     }
 })
